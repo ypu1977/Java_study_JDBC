c78b5b8870ccf2663540dbb13ea12a1945c1dfd9
ypu1977 <ypu1977@naver.com>
2022-07-27 12:20:59 +0900

    1

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -1,9 +1,19 @@
 package com.iu.test;
 
+import com.iu.util.DBConnector;
+
 public class TestMain 
 {
 	public static void main(String[] args) 
 	{
-		
+		DBConnector dbConnector = new DBConnector();
+		try
+		{
+			dbConnector.getConnection();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
